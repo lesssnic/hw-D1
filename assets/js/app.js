aae1853c7c8884401944da6fc0df4b1d776527de
lesssnic <antoxxxa.ua@gmail.com>
2021-06-21 15:04:32 +0300

    add-years

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,21 @@
 
 
+function validateInn(inn = innCode.value){
 
+let firstTimeStamp = new Date(1900, 0, 0).getTime();
 
-function validateInn(){
+let birthdayTimeStamp = (+inn.slice(0, 5)) * 86400000 + firstTimeStamp;
 
-let inn = innCode.value;
+let novaTimeStamp = new Date().getTime();
 
-let birthdayPart = +inn.slice(0, 5);
+let fullYears = (new Date(firstTimeStamp + (novaTimeStamp - birthdayTimeStamp)).getYear());
+
+let birthday = `${(new Date(birthdayTimeStamp).getFullYear())}-${(new Date(birthdayTimeStamp).getMonth()+1)}-${(new Date(birthdayTimeStamp).getDate())}`;
 
 let k = 0;
 
 for (i = 0; i < inn.length - 1; i++){
-
     let control = [-1, 5, 7, 9, 4, 6, 10 , 5, 7];
-
     k += +inn[i] * control[i];
 }
 
@@ -23,20 +25,12 @@ let sex = 0;
 
 let valid = 0;
 
-let ms = birthdayPart * 86400000 - 2209075200000;
-
-let birthday = new Date(ms);
-
 if (kn == +inn[inn.length-1]){
     valid = 'Код валиден';
     if (+inn[inn.length-2]%2 == 1){
         sex = 'Мужчина';
     }else sex = 'Женщина';
-    resultValidation.innerHTML = `<tr><th>${inn}</th><th>${valid}</th><th>${sex}</th><th>${birthday}</th></tr>`;
-}else resultValidation.innerHTML = `<tr><th>${inn}</th><th>Ошибка в коде</th><th></th><th></th></tr>`;
-
-
-
-    
+    resultValidation.innerHTML = `<tr><th>${inn}</th><th>${valid}</th><th>${sex}</th><th>${birthday}</th><th>${fullYears}</th></tr>`;
+}else resultValidation.innerHTML = `<tr><th>${inn}</th><th>Ошибка в коде</th><th></th><th></th><th></th></tr>`;
 }
 
